5b5b739c562af67c2a3f37ebd9b698b6258caf20
Paul Nation <nonhermitian@gmail.com>
2024-02-02 14:01:16 -0500

    run black

--- fulqrum_tophat/__init__.py
+++ fulqrum_tophat/__init__.py
@@ -8,7 +8,7 @@ try:
     from .version import version as __version__
     from .version import openmp
 except ImportError:
-    __version__ = '0.0.0'
+    __version__ = "0.0.0"
     openmp = False
 
 from .core.qubit_operator import QubitOperator
