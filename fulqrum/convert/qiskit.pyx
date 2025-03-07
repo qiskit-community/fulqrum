11e57bda4bee536c2f507a74d449ca03e7345e7d
Paul Nation <nonhermitian@gmail.com>
2025-03-07 17:06:32 -0500

    SparsePauliOp to Fulqrum

--- fulqrum/convert/qiskit.pyx
+++ fulqrum/convert/qiskit.pyx
@@ -1,17 +1,13 @@
-# Fulqrum - Top Hat
+# Fulqrum
 # Copyright (C) 2024, IBM
 # cython: c_string_type=unicode, c_string_encoding=UTF-8
 cimport cython
-from libcpp.string cimport string
 from fulqrum.core.qubit_operator cimport QubitOperator
-from fulqrum.core.qubit_operator cimport STR_TO_IND as QUBIT_STR_TO_IND
-from fulqrum.core.base cimport OperatorTerm
 
-cdef const OperatorTerm EmptyOperatorTerm
 
 
 @cython.boundscheck(False)
-def SparsePauliOp_to_QubitOperator(object op):
+def sparsepauli_to_fulqrum(object op):
     """Convert a Qiskit SparsePauliOp to QubitOperator
 
     Parameters:
@@ -20,21 +16,9 @@ def SparsePauliOp_to_QubitOperator(object op):
     Returns:
         QubitOperator: Converted operator
     """
-    cdef OperatorTerm term
-    cdef str string, s
+    cdef str label
     cdef complex coeff
-    cdef size_t idx = 0
     cdef QubitOperator out = QubitOperator(op.num_qubits)
-    for string, coeff in op.label_iter():
-        term = EmptyOperatorTerm
-        term.coeff = coeff
-        idx = 0
-        for s in string[::-1]:
-            if s != 'I':
-                term.indices.push_back(idx)
-                term.indices.push_back(QUBIT_STR_TO_IND[s])
-            idx += 1
-        out.terms.push_back(term)
-    out.sorted = True
+    for label, coeff in op.label_iter():
+        out += QubitOperator.from_label(label, coeff)
     return out
-
