78a45709d59d5991f8f26faf0e9364cca9641ba0
Paul Nation <nonhermitian@gmail.com>
2025-03-13 15:26:30 -0400

    Add CSR files

--- fulqrum/core/subspace.pyx
+++ fulqrum/core/subspace.pyx
@@ -10,7 +10,11 @@ cimport cython
 import numpy as np
 cimport numpy as np
 
+from fulqrumc.core.matrix import FulqrumCSR
+from fulqrum.core.spmv cimport FulqrumSpMV
+
 include "fulqrum/core/includes/bitstrings_header.pxi"
+include "fulqrum/core/includes/csr_header.pxi"
 
 
 cdef size_t intmin(size_t a, size_t b):
@@ -169,4 +173,77 @@ cdef class Subspace():
             for idx in range(self.subspace.num_qubits):
                 temp[idx] = sub[kk*self.subspace.num_qubits+idx] + 48
             out[temp] = None
+        return out
+
+    def to_csr(self):
+        cdef FulqrumSpMV spmv = self.spmv
+        cdef size_t max_int = np.iinfo(np.int32).max
+        cdef object out
+        cdef size_t num_terms = spmv.oper.terms.size()
+        cdef int inds_64 = 0
+        if (spmv.subspace_dim > max_int):
+            inds_64 = 1
+        if num_terms:
+            if (spmv.subspace_dim*spmv.oper.terms[num_terms-1].group > max_int):
+                inds_64 = 1
+        cdef int[::1] indptr32
+        cdef int[::1] indices32
+        cdef long long[::1] indptr64
+        cdef long long[::1] indices64
+
+        if inds_64:
+            indptr64 = np.zeros(spmv.subspace_dim+1, dtype=np.int64)
+            indices64 = np.zeros(1, dtype=np.int64)
+        else:
+            indptr32 = np.zeros(spmv.subspace_dim+1, dtype=np.int32)
+            indices32 = np.zeros(1, dtype=np.int32)
+        
+        cdef double complex[::1] data = np.zeros(1, dtype=complex)
+        cdef int compute_values = 0
+        
+        if spmv.diag_vec.shape[0] == 0 and spmv.has_nonzero_diag:
+                spmv.compute_diag_vector()
+        if inds_64:
+            csr_builder[int64](spmv.oper, spmv.subspace.subspace.bitstrings, &spmv.diag_vec[0],
+                            spmv.width, spmv.subspace_dim, spmv.has_nonzero_diag,
+                            spmv.bin_width, spmv.bin_ranges,
+                            &indptr64[0], &indices64[0], &data[0],
+                            compute_values)
+            if not indptr64[spmv.subspace_dim]:
+                return FulqrumCSR(([], [[],[]]), shape=(spmv.subspace_dim,)*2, dtype=complex)
+            indices64 = np.zeros(indptr64[spmv.subspace_dim], dtype=np.int64)
+            data = np.zeros(indptr64[spmv.subspace_dim], dtype=complex)
+        else:
+            csr_builder[int](spmv.oper, spmv.subspace.subspace.bitstrings, &spmv.diag_vec[0],
+                            spmv.width, spmv.subspace_dim, spmv.has_nonzero_diag,
+                            spmv.bin_width, spmv.bin_ranges,
+                            &indptr32[0], &indices32[0], &data[0],
+                            compute_values)
+            if not indptr32[spmv.subspace_dim]:
+                return FulqrumCSR(([], [[],[]]), shape=(spmv.subspace_dim,)*2, dtype=complex)
+            indices32 = np.zeros(indptr32[spmv.subspace_dim], dtype=np.int32)
+            data = np.zeros(indptr32[spmv.subspace_dim], dtype=complex)
+
+        compute_values = 1
+        if inds_64:
+            csr_builder[int64](spmv.oper, spmv.subspace.subspace.bitstrings, &spmv.diag_vec[0],
+                            spmv.width, spmv.subspace_dim, spmv.has_nonzero_diag,
+                            spmv.bin_width, spmv.bin_ranges,
+                            &indptr64[0], &indices64[0], &data[0],
+                            compute_values)
+        else:
+            csr_builder[int](spmv.oper, spmv.subspace.subspace.bitstrings, &spmv.diag_vec[0],
+                            spmv.width, spmv.subspace_dim, spmv.has_nonzero_diag,
+                            spmv.bin_width, spmv.bin_ranges,
+                            &indptr32[0], &indices32[0], &data[0],
+                            compute_values)
+        
+        if inds_64:
+            out = FulqrumCSR((np.asarray(data), np.asarray(indices64), np.asarray(indptr64)),
+                            shape=(spmv.subspace_dim,)*2, dtype=complex)
+        else:
+            out = FulqrumCSR((np.asarray(data), np.asarray(indices32), np.asarray(indptr32)),
+                            shape=(spmv.subspace_dim,)*2, dtype=complex)
+        # Indices need not be in order, so sort here
+        out.sort_indices()
         return out
