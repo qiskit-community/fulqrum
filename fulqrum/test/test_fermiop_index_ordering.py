0dc4fd190b103cebc5a1f1bf23b933a0f4309bc2
Paul Nation <nonhermitian@gmail.com>
2025-03-26 11:24:47 -0400

    indexing tests

---
+++ fulqrum/test/test_fermiop_index_ordering.py
@@ -0,0 +1,39 @@
+# Fulqrum
+# Copyright (C) 2024, IBM
+# pylint: disable=no-name-in-module
+"""Test index ordering of FermionicOperator terms"""
+
+from fulqrum import FermionicOperator
+
+
+def test_index_ordering_simple1():
+    """Test index ordering of single term FermionicOperator"""
+    N = 5
+    fo = FermionicOperator(N, [("-++-+", [3, 2, 3, 0, 0])])
+    fo.index_ordering()
+    assert fo.operators == [("-", 0), ("+", 0), ("+", 2), ("-", 3), ("+", 3)]
+
+
+def test_index_ordering_simple2():
+    """Test index ordering of single term FermionicOperator"""
+    N = 5
+    fo = FermionicOperator(N, [("-++-+", [2, 2, 3, 0, 0])])
+    fo.index_ordering()
+    assert fo.operators == [("-", 0), ("+", 0), ("-", 2), ("+", 2), ("+", 3)]
+
+
+def test_index_ordering_preservation():
+    """Test index ordering does not exchange terms with same indices"""
+    N = 5
+    fo = FermionicOperator(N, [("-++--++-", [2, 2, 0, 0, 3, 3, 1, 1])])
+    fo.index_ordering()
+    assert fo.operators == [
+        ("+", 0),
+        ("-", 0),
+        ("+", 1),
+        ("-", 1),
+        ("-", 2),
+        ("+", 2),
+        ("-", 3),
+        ("+", 3),
+    ]
