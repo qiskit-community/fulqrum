5ed74279070c3100dc488463ad2ac1b64e2df845
Paul Nation <nonhermitian@gmail.com>
2025-05-21 14:05:51 -0400

    black

--- fulqrum/test/test_bitset.py
+++ fulqrum/test/test_bitset.py
@@ -32,17 +32,17 @@ def test_to_int1():
 
 def test_to_int2():
     """Test two different len bitsets that equal same int"""
-    string1 = '101110'
+    string1 = "101110"
     bits1 = Bitset(string1)
 
-    string2 = '0101110'
+    string2 = "0101110"
     bits2 = Bitset(string2)
     assert bits1.to_int() == bits2.to_int()
 
 
 def test_equality():
     """Test Bitset equality"""
-    string1 = '101110'
+    string1 = "101110"
     bits1 = Bitset(string1)
     bits2 = Bitset(string1)
 
@@ -51,10 +51,10 @@ def test_equality():
 
 def test_inequality1():
     """Test Bitset inequality, same length"""
-    string1 = '101110'
+    string1 = "101110"
     bits1 = Bitset(string1)
 
-    string2 = '101111'
+    string2 = "101111"
     bits2 = Bitset(string2)
 
     assert bits1 != bits2
@@ -62,10 +62,10 @@ def test_inequality1():
 
 def test_inequality2():
     """Test Bitset inequality, different lengths"""
-    string1 = '101110'
+    string1 = "101110"
     bits1 = Bitset(string1)
 
-    string2 = '0101110'
+    string2 = "0101110"
     bits2 = Bitset(string2)
 
     assert bits1 != bits2
@@ -73,17 +73,17 @@ def test_inequality2():
 
 def test_bin_width_int1():
     """Test bin-width integers are correct (small)"""
-    string = '101110'
+    string = "101110"
     bits = Bitset(string)
     ans = [0, 2, 6, 14, 14, 46]
-    for kk in range(1,7):
+    for kk in range(1, 7):
         out = bits.bin_width_int(kk)
-        assert out == ans[kk-1]
+        assert out == ans[kk - 1]
 
 
 def test_bin_width_int2():
     """Test bin-width integers are correct (large)"""
-    string = '101110' * 100
+    string = "101110" * 100
     str_len = len(string)
     bits = Bitset(string)
     for kk in range(30, 41):
