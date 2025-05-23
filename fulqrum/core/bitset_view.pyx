614f2623bba7cbd5c3170671d7808daa98fcb481
Paul Nation <nonhermitian@gmail.com>
2025-05-23 14:36:30 -0400

    add to bitsetview

--- fulqrum/core/bitset_view.pyx
+++ fulqrum/core/bitset_view.pyx
@@ -4,6 +4,10 @@ from .bitset cimport bitset_t, to_string
 from .bitset_view cimport BitsetView
 from libcpp.string cimport string
 
+from fulqrum.exceptions import FulqrumError
+
+include "includes/bitset_utils_header.pxi"
+
 
 cdef class BitsetView:
         
@@ -19,6 +23,11 @@ cdef class BitsetView:
         return f"<BitsetView: {s}>"
 
     def size(self):
+        """Number of bits in Bitset
+
+        Returns:
+            int: Number of bits
+        """
         return self.bits.size()
 
     def __getitem__(self, size_t idx):
@@ -42,3 +51,28 @@ cdef class BitsetView:
         cdef string s
         to_string(deref(self.bits), s)
         return s
+
+    def to_int(self):
+        """Convert Bitset to Python integer
+
+        Returns:
+            int: Integer value for Bitset
+        """
+        cdef string s
+        to_string(deref(self.bits), s)
+        return int(s, 2)
+
+    def bin_width_int(self, unsigned int bin_width):
+        """Compute the integer for a given bin-width
+
+        Parameters:
+            bin_width (int): Bin-width to compute
+
+        Returns:
+            int: Computed bin-width
+        """
+        if bin_width > self.bits.size():
+            raise FulqrumError("bin_width is larger than number of bits")
+        cdef size_t out
+        bin_int(deref(self.bits), bin_width, out)
+        return out
