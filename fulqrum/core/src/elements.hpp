45d75b0f9198f30b2593bc188b073f601b979f20
Paul Nation <nonhermitian@gmail.com>
2025-05-29 14:25:47 -0400

    start adding bitset element funcs

--- fulqrum/core/src/elements.hpp
+++ fulqrum/core/src/elements.hpp
@@ -2,9 +2,15 @@
  * Fulqrum
  * Copyright (C) 2024, IBM
  */
+#ifndef BOOST_DYNAMIC_BITSET_DONT_USE_FRIENDS
+#define BOOST_DYNAMIC_BITSET_DONT_USE_FRIENDS
+#endif
 #pragma once
 #include <cstdlib>
 #include <complex>
+#include <boost/dynamic_bitset.hpp>
+
+const unsigned int BITS_PER_BLOCK = 8 * sizeof(std::size_t);
 
 
 const std::complex<double> OPER_ELEMENTS[28] = {{1,0}, {0,0}, {0,0}, {-1,0},   // Z
@@ -35,7 +41,7 @@ inline void accum_element_value(const unsigned char *__restrict row,
                                 const unsigned int bit_len,
                                 const unsigned int *__restrict pos,
                                 const unsigned char *__restrict val,
-                                std::complex<double> coeff,
+                                const std::complex<double> coeff,
                                 const std::size_t N,
                                 std::complex<double>& out)
     {
@@ -44,6 +50,32 @@ inline void accum_element_value(const unsigned char *__restrict row,
         int offset;
         for(kk=0; kk<N; kk++){
             offset = 2*(row[bit_len - pos[kk] - 1]) + (col[bit_len - pos[kk] - 1]);
+            temp *= OPER_ELEMENTS[4*val[kk] + offset];
+        }
+        out += coeff*temp;
+    }
+
+
+inline void accum_element(const boost::dynamic_bitset<std::size_t>& row,
+                          const boost::dynamic_bitset<std::size_t>& col,
+                          const unsigned int bit_len,
+                          const unsigned int *__restrict inds,
+                          const unsigned char *__restrict val,
+                          const std::complex<double>& coeff,
+                          const unsigned int N,
+                          std::complex<double>& out)
+    {
+        std::complex<double> temp = {1.0, 0};
+        std::size_t kk;
+        unsigned int offset, block_num, block_idx;
+        unsigned int pos, row_int, col_int;
+        for(kk=0; kk<N; kk++){
+            pos = inds[kk];
+            block_num = pos / BITS_PER_BLOCK;
+            block_idx = pos % BITS_PER_BLOCK;
+            row_int = (row.m_bits[block_num] >> block_idx) & 1U;
+            col_int = (col.m_bits[block_num] >> block_idx) & 1U;
+            offset = 2*row_int + col_int;
             temp *= OPER_ELEMENTS[4*val[kk] + offset];
         }
         out += coeff*temp;
