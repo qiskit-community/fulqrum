d35d960f825bfa664331180c41399a6dd3a7cea2
Paul Nation <nonhermitian@gmail.com>
2025-06-04 15:17:50 -0400

    Reduce repr size to at most 100 terms

--- fulqrum/core/fermi_operator.pyx
+++ fulqrum/core/fermi_operator.pyx
@@ -253,6 +253,11 @@ cdef class FermionicOperator():
         cdef FermionicTerm_t term
         cdef size_t kk
         cdef size_t num_terms = self.oper.terms.size()
+        cdef size_t total_terms = num_terms
+        cdef int too_many_terms = 0
+        if num_terms > 100:
+            too_many_terms = 1
+            num_terms = 100
         for idx in range(num_terms):
             temp_str = ''
             term = self.oper.terms[idx]
@@ -264,6 +269,8 @@ cdef class FermionicOperator():
             out.append((temp_str, term.coeff))
 
         out_strs = ', '.join(str(kk) for kk in out)
+        if too_many_terms:
+            out_strs += f' + {total_terms-100} more terms'
         return f"<FermionicOperator[{out_strs}], width={self.oper.width}>"
 
 
