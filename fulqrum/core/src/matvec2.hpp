59376fe17e115d74dbcee05d71a94b3fc47cd2f9
Paul Nation <nonhermitian@gmail.com>
2025-07-16 14:32:14 -0400

    add description

--- fulqrum/core/src/matvec2.hpp
+++ fulqrum/core/src/matvec2.hpp
@@ -91,7 +91,7 @@ void omp_matvec2(const std::vector<OperatorTerm_t>& terms,
                                   term->coeff, term->indices.size(), temp_val);
                     }
                 } // end loop over this group
-                if(!do_col_search)
+                if(!do_col_search) // if at least one element was found
                 {
                     val += temp_val * in_vec[col_idx];
                 }
