9c8f8d0cdae9b0fd70f940189c8715085a6ce0ec
Paul Nation <nonhermitian@gmail.com>
2025-12-10 08:42:31 -0500

    Add docstrings

--- fulqrum/core/src/diag.hpp
+++ fulqrum/core/src/diag.hpp
@@ -15,6 +15,17 @@
 #include <boost/dynamic_bitset.hpp>
 
 
+/**
+ * Populate the diagonal vector for a given diagonal operator
+ *
+ *
+ * @param data The subspace data
+ * @param diag_vec The diagonal vector to store information to
+ * @param val Variable storing the element value
+ * @param diag_oper The diagonal operator
+ * @param width The width of the operator
+ * @param subspace_dim The dimension of the subspace
+ */
 template <typename T>
 void compute_diag_vector(const bitset_map_namespace::BitsetHashMapWrapper &data,
                          T *__restrict diag_vec,
@@ -36,6 +47,14 @@ void compute_diag_vector(const bitset_map_namespace::BitsetHashMapWrapper &data,
 }
 
 
+/**
+ * Compute the diagonal matrix-element for a single bit-string
+ *
+ *
+ * @param row The row bit-string
+ * @param diag_oper The diagonal operator
+ * @param val Variable storing the element value
+ */
 template <typename T>
 inline void single_bitstring_diagonal(const boost::dynamic_bitset<size_t>& row,
                                       const QubitOperator_t &diag_oper,
