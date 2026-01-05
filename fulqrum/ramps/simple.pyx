3e4a5772c3b528e8e364d6285caa053e118aa9de
Paul Nation <nonhermitian@gmail.com>
2026-01-05 12:51:27 -0500

    cpp headers

--- fulqrum/ramps/simple.pyx
+++ fulqrum/ramps/simple.pyx
@@ -1,5 +1,14 @@
-# Fulqrum
-# Copyright (C) 2024, IBM
+# This code is a Qiskit project.
+#
+# (C) Copyright IBM 2024.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
 # cython: c_string_type=unicode, c_string_encoding=UTF-8
 
 from fulqrum.core.qubit_operator cimport QubitOperator
