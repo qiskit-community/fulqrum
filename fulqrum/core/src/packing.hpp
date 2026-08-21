1d628ac6087039e88ae6c76aee9e25fd689552c7
Paul Nation <nonhermitian@gmail.com>
2026-08-21 10:20:32 -0400

    add docstrings

--- fulqrum/core/src/packing.hpp
+++ fulqrum/core/src/packing.hpp
@@ -17,20 +17,33 @@
 #include "constants.hpp"
 
 
-
+/**
+ * Pack operator index and value into a single uint32
+ *
+ * @param ind The index on which the operator acts
+ * @param val Operator value
+ */
 inline std::uint32_t pack_data(const width_t ind, const unsigned char val)
 {
     return (static_cast<std::uint32_t>(ind) << 8) | val;
 }
 
-
-inline width_t get_ind(const std::uint32_t packed_val)
+/**
+ * Extract operator index from a packed uint32
+ *
+ * @param packed_data Packed index and operator value
+ */
+inline width_t get_ind(const std::uint32_t packed_data)
 {
-    return static_cast<width_t>((packed_val & 0xFFFFFF00) >> 8);
+    return static_cast<width_t>((packed_data & 0xFFFFFF00) >> 8);
 }
 
-
-inline unsigned char get_val(const std::uint32_t packed_val)
+/**
+ * Extract operator value from a packed uint32
+ *
+ * @param packed_data Packed index and operator value
+ */
+inline unsigned char get_val(const std::uint32_t packed_data)
 {
-    return static_cast<unsigned char>(packed_val & 0x000000FF);
+    return static_cast<unsigned char>(packed_data & 0x000000FF);
 }
