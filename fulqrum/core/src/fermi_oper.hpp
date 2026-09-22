912591251f28f8fffad927d4291a2de9607538ea
Paul Nation <nonhermitian@gmail.com>
2026-09-22 14:37:43 -0400

    clang format

--- fulqrum/core/src/fermi_oper.hpp
+++ fulqrum/core/src/fermi_oper.hpp
@@ -494,17 +494,15 @@ inline void set_fermi_sorting_flags(FermionicOperator& oper, std::string kind)
     }
 }
 
-
 #include "../../convert/src/fcidump.hpp"
 #include "../../convert/src/integrals.hpp"
-inline FermionicOperator FermionicOperator::from_fcidump(std::string filename, double tol=1e-12)
-    {
-        FCIDumpData_t data = parse_fcidump(filename);
-        int norb = data.NORB;
-        int norb2 = norb * norb;
-        std::vector<double> two_body_ints = data.two_body_integrals(true);
-        FermionicOperator out = pyscf_integrals_to_fermionic(&data.H1[0], &two_body_ints[0],
-                                                             norb2, norb2*norb2, 
-                                                             data.ECORE, tol);
-        return out;
-    }
+inline FermionicOperator FermionicOperator::from_fcidump(std::string filename, double tol = 1e-12)
+{
+    FCIDumpData_t data = parse_fcidump(filename);
+    int norb = data.NORB;
+    int norb2 = norb * norb;
+    std::vector<double> two_body_ints = data.two_body_integrals(true);
+    FermionicOperator out = pyscf_integrals_to_fermionic(
+        &data.H1[0], &two_body_ints[0], norb2, norb2 * norb2, data.ECORE, tol);
+    return out;
+}
